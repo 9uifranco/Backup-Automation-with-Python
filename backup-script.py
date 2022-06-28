e78f65cd1d80b9e6ce66f5c4d416b549b42859b5
Guilherme Franco <9uifranco@gmail.com>
2022-06-28 10:20:20 -0300

    Add backup code

--- backup-script.py
+++ backup-script.py
@@ -1,7 +1,7 @@
 # core python
 
 import os
-import os.auth
+import os.path
 
 # google stuffs
 
@@ -20,7 +20,7 @@ from googleapiclient.http import MediaFileUpload
 # if you add more scopes, you gonna need refresh the token (delete it & create a new one)
 
 
-SCOPES = ["https://wwww.googleapis.com/auth/drive"] 
+SCOPES = ['https://wwww.googleapis.com/auth/drive'] 
 
 creds = None
 
@@ -42,5 +42,43 @@ if not creds or not creds.valid:
     with open('token.json', 'w') as token: # create the token file with writing mode
         token.write(creds.to_json()) # taking that object and putting into a json file
 
+# ------- BACKING UP FILES -------- #
 
+try:
+    # create a service and try to access the google drive api with our authorization
+    service = build("drive", "v3", credentials=creds)
+    # do the request and get the response
+    response = service.files().list(
+        q="name='BackupFolder' and mimeType='application/vnd.google-apps.folder'",
+        spaces='drive'
+    ).execute()
+    # if there's no folder, we r going to create a new one
+    if not response['files']:
+        # metadata
+        file_metadata = {
+            "name": "BackupFolder",
+            "mimeType": "application/vnd.google-apps.folder"
+        }
+        # create the file
+        file = service.files().create(body=file_metadata, fields="id").execute()
+        # get the folder id to put stuff in it
+        folder_id = file.get('id')
+    # if the folder exist
+    else:
+        folder_id = response['files'][0]['id']
+    # iterate through the files we have in the local bakcup folder and upload to the remote folder
+    for file in os.listdir('backup-1'): # list all the files in the directory
+        file_metadata = {
+            "name": file,
+            "parents": [folder_id] 
+        }
 
+        media = MediaFileUpload(f"backup-1/{file}")
+        upload_file = service.files().create(body=file_metadata,
+                                            media_body=media,
+                                            fields="id").execute()
+        
+        print("Backed up file: " + file)
+
+except HttpError as e:
+    print("Error: " + str(e))
